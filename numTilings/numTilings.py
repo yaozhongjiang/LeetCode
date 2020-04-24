e16b0942a8c0b856f04bafe5f22045ff2c53989c
Roger <yaozj_real@yeah.net>
2020-04-25 00:35:24 +0800

    solution to path sum problem 2

---
+++ numTilings/numTilings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def numTilings(self, N: int) -> int:
+        return 0
+
+
+def main():
+    num = 3
+    solution = Solution()
+    sol = solution.numTilings(num)
+    print(sol)
+
+
+if __name__ == '__main__':
+    main()
+
--- pathSumII/pathSumII.py
+++ pathSumII/pathSumII.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import copy
 from typing import List
 
 
@@ -11,16 +12,47 @@ class TreeNode:
 
 class Solution:
     def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
-        list_list = [[]]
-        return list_list
+        if root is None:
+            return []
+        if root.left is None and root.right is None:
+            if root.val == sum:
+                return [[root.val]]
+            else:
+                return []
+        list_sum = sum
+        list_depth = [root.val]
+        final = []
+        if root.left is not None:
+            self.visit_depth(root.left, list_sum, list_depth, final)
+        if root.right is not None:
+            self.visit_depth(root.right, list_sum, list_depth, final)
+        return final
+
+    def visit_depth(self, left: TreeNode, list_sum, list_depth, final):
+        if left.left is None and left.right is None:
+            list_depth.append(left.val)
+            if sum(list_depth) == list_sum:
+                final.append(copy.deepcopy(list_depth))
+            list_depth.pop(-1)
+
+        if left.left is not None:
+            list_depth.append(left.val)
+            self.visit_depth(left.left, list_sum, list_depth, final)
+            list_depth.pop(-1)
+        if left.right is not None:
+            list_depth.append( left.val )
+            self.visit_depth(left.right, list_sum, list_depth, final)
+            list_depth.pop(-1)
 
 
 def create_tree(nodes) -> TreeNode:
+    if nodes is None or len(nodes) == 0:
+        return TreeNode(0)
     root = TreeNode(nodes[0])
     queue = [root]
     for i in range(1, len(nodes)):
         if nodes[i] is None:
-            tree = None
+            tree = TreeNode(0)
         else:
             tree = TreeNode(nodes[i])
         if i % 2 == 1:
@@ -46,10 +78,16 @@ def visit_node(node: TreeNode, depth):
 
 
 def main():
-    nodes = [3, 5, 1, 6, 2, 0, 8, None, None, 7, 4]
+    # nodes = [5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, None, 5, 1]
+    # sum = 22
+    # nodes = []
+    # sum = 1
+    nodes = [1, 2]
+    sum = 0
     root = create_tree(nodes)
+    # visit_node(root, 0)
     solution = Solution()
-    sol = solution.pathSum(root)
+    sol = solution.pathSum(root, sum)
     print(sol)
 
 
