e34e75a9b872d1d142f0fbee4303a93564aa1a4c
Roger <yaozj_real@yeah.net>
2020-04-28 00:41:58 +0800

    solution to number tilings

--- numTilings/numTilings.py
+++ numTilings/numTilings.py
@@ -2,7 +2,18 @@
 
 class Solution:
     def numTilings(self, N: int) -> int:
-        return 0
+        a = [ 0 for i in range(0, N + 2)]
+        b = [ 0 for i in range(0, N + 2)]
+        c = 1e9 + 7
+        a[1] = 1
+        a[2] = 2
+        b[2] = 1
+        if N >= 3:
+            for i in range(3, N+1):
+                a[i]=(a[i-1]+a[i-2]+2 * b[i-1]) % c
+                b[i]=(b[i-1]+a[i-2]) % c
+                i += 1
+        return int(a[N])
 
 
 def main():
---
+++ sortColors/sortColors.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+
+def main():
+    nums = []
+    solution = Solution()
+    solution.sortColors(nums)
+
+
+if __name__ == '__main__':
+    main()
+
