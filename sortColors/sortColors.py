bc969c261aec387a6e79034aa61bf8ee7e548989
Roger <yaozj_real@yeah.net>
2020-04-28 13:07:33 +0800

    solution to sort colors

--- sortColors/sortColors.py
+++ sortColors/sortColors.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
+from typing import List
+
 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] < nums[j]:
+                    temp = nums[i]
+                    nums[i] = nums[j]
+                    nums[j] = temp
+        print(nums)
 
 
 def main():
-    nums = []
+    nums = [2,0,2,1,1,0]
     solution = Solution()
     solution.sortColors(nums)
 
